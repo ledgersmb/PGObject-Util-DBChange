f5b0e5a20d30685990ac667bfc608f31e522bdbb
Chris Travers <chris@efficito.com>
2016-03-07 20:36:48 +0100

    more scaffolding, next is copy/paste and testing of this part, before publication

--- lib/PGObject/Util/DBChange.pm
+++ lib/PGObject/Util/DBChange.pm
@@ -4,6 +4,12 @@ use 5.006;
 use strict;
 use warnings;
 
+use strict;
+use warnings;
+use Digest::SHA;
+use Cwd;
+use Moo;
+
 =head1 NAME
 
 PGObject::Util::DBChange - The great new PGObject::Util::DBChange!
@@ -28,10 +34,97 @@ Perhaps a little code snippet.
     my $foo = PGObject::Util::DBChange->new();
     ...
 
-=head1 EXPORT
+=head1 PROPERTIES
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head2 path
+
+Path to load content from -- Must be defined and '' or a string
+
+=cut
+
+has path => (is => 'ro'
+             isa => sub { die 'path undefined' unless defined $_[0]; 
+                          die 'references not allowed' if ref $_[0]; } );
+
+=head2 content
+
+Content of the file.  Can be specified at load, or is built by reading from the
+file.
+
+=cut
+
+has content => (is => 'lazy');
+
+sub _build_content {
+    my ($self) = @_;
+    my $file;
+    local $!;
+    open(FILE, '<', $self->path) or
+        die 'FileError: ' . Cwd::abs_path($self->path) . ": $!";
+    binmode FILE, ':utf8';
+    $content = join '', <FILE>;
+    close FILE;
+    return $content;
+}
+
+=head2 dependencies
+
+A list of other changes to apply first.  If strings are provided, these are
+turned into path objects.
+
+Currently these must be explicitly provided. Future bersions may read these from
+comments in the files themselves.
+
+=cut
+
+has dependencies => (is => 'ro',
+                     default => sub { [] },
+                     isa => sub {  die 'dependencies must be an arrayref' 
+                                           unless ref $_[0] =~ /ARRAY/;
+                                   for (@{$_[0]}) {
+                                       die 'dependency must be a PGObject::Util::Change object'
+                                           unless eval { $_->isa(__PACKAGE__) };
+                                   }
+                           }
+                    );
+                                           
+
+=head2 sha
+
+The sha hash of the normalized content (comments and whitespace lines stripped)
+of the file.
+
+=cut
+
+has sha => (is => 'lazy');
+
+sub _build_sha {
+    my ($self) = @_;
+    my $content = $self->content; 
+    my $normalized = join "\n",
+                     grep { /\S/ }
+                     map { my $string = $_; $string =~ s/--.*//; $string }
+                     split("\n", $content);
+    return Digest::SHA::sha512_base64($normalized);
+}
+
+=head2 begin_txn
+
+Code to begin transaction, defaults to 'BEGIN;'
+
+=cut
+
+has begin_txn => is ('ro', default => 'BEGIN;');
+
+=head2 commit_txn
+
+Code to commit transaction, defaults to 'COMMIT;'
+
+Useful if one needs to do two phase commit or similar
+
+=cut
+
+has commit_txn => is ('ro', default => 'COMMIT;');
 
 =head1 SUBROUTINES/METHODS
 
@@ -94,6 +187,7 @@ L<http://search.cpan.org/dist/PGObject-Util-DBChange/>
 
 =head1 ACKNOWLEDGEMENTS
 
+Sedex Global sponsored part of this development.
 
 =head1 LICENSE AND COPYRIGHT
 
